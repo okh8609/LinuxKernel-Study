27dcbe89279aceca4f0e82d9f07b7cc10d92f289
khaos <okh8609@gmail.com>
2022-04-14 08:37:45 +0800

    交叉編譯 (Cross compile)

--- CH1-5-3/CMD.sh
+++ CH1-5-3/CMD.sh
@@ -3,3 +3,5 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 export BASEINCLUDE=~/runninglinuxkernel_5.0
 
 cp -R ./CH1-5-3 ../runninglinuxkernel_5.0/kmodules/ 
+
+# apt install linux-headers-5.16.0-6-arm64
